5a23ec299f7dfd3f6264717f4d46c82333763d6f
mno01 <42007873+mno01@users.noreply.github.com>
2018-08-24 16:57:54 -0700

    ProgrammingAssignment2
    Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+#8/24/18 
+#These functions cache the inverse of a matrix, so that when 
+#it is needed again, the inverse can be pulled the cache instead
+#of being recalculated.  This will speed up an otherwise lengthy
+##process.
 
 
-## Write a short comment describing this function
+##The makeCacheMatrix function creates a list of functions 
+##including: 
+##set --set matrix input
+##get --get matrix input
+##setsolve --set inverse of the matrix
+##getsolve --get inverse of the matrix
+
+
+makeCacheMatrix <- function(x = matrix()) { 
+  m <- NULL 
+  set <- function(y) { 
+    x <<- y 
+    m <<- NULL 
+  } 
+  get <- function() x 
+  setsolve <- function(solve) { 
+    m <<- solve 
+  } 
+  getsolve <- function() m 
+  
+  list(set = set, get = get, 
+       setsolve = setsolve, 
+       getsolve = getsolve) 
+} 
+
+
+
+
+##The cacheSolve function checks to see if the inverse
+## has been calculated.  If not, it calculates the inverse 
+##of the matrix.  If it has been calculated, it retrieves the 
+##inverse matrix from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+
+
+## Try it out!
+
+## Here is a matrix to test with:
+TestMatrix <- matrix(c(1, 2, 3, 4), nrow=2, ncol=2)
+
+
+#Applying matrix to functions
+cacheSolve(makeCacheMatrix(TestMatrix)) 
+
+
+
+
+
+
